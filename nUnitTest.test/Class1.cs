5c5098920f63ef5123d43c7f6387391803f1f9f1
donguklee <donguklee@fashiongo.net>
2014-10-02 09:14:22 -0700

    1002test

---
+++ nUnitTest.test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nUnitTest.test
+{
+    public class Class1
+    {
+    }
+}
